e215771dcecafbf16ae7da3d53f430cf4e02a037
Ryuichi Yamafuji Lun <92515437+Ryuichi-Yamafuji-Lun@users.noreply.github.com>
2025-10-19 11:03:37 -0700

    installation of tetris gymnasium

---
+++ quickstart.py
@@ -0,0 +1,16 @@
+import gymnasium as gym
+
+from tetris_gymnasium.envs.tetris import Tetris
+
+if __name__ == "__main__":
+    # creates tetris env
+    env = gym.make("tetris_gymnasium/Tetris", render_mode="ansi")
+    # ensures that blocks are the same for testing
+    env.reset(seed=42)
+
+    terminated = False
+    while not terminated:
+        print(env.render() + "\n")
+        action = env.action_space.sample()
+        observation, reward, terminated, truncated, info = env.step(action)
+    print("Game Over!")
