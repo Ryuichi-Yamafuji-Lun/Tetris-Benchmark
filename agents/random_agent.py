f6663b79e7ea1698bf5c04e9079db238e1d60247
Ryuichi Yamafuji Lun <92515437+Ryuichi-Yamafuji-Lun@users.noreply.github.com>
2025-10-19 23:40:49 -0700

    updated the random agent withthe new observation space

--- agents/random_agent.py
+++ agents/random_agent.py
@@ -1,4 +1,7 @@
 # random agent
+import numpy as np
 
-def agent_action(env):
-    return env.action_space.sample()
+def agent_action(obs, info):
+    action_mask = info["action_mask"]
+    valid_actions = np.where(action_mask == 1)[0]
+    return np.random.choice(valid_actions)
